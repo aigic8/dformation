68a0b2bbafe11cd0e5a8a4c6023a9d764d6b7074
aigic8 <aigic.dev@protonmail.com>
2025-02-04 01:34:48 +0330

    testing deployment

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,10 @@ export default function Home() {
             </code>
             .
           </li>
-          <li>Save and see your changes instantly.</li>
+          <li className="mb-2">Save and see your changes instantly.</li>
+          <li>
+            Testing prod but <b>CAN YOU SEE ME?</b>
+          </li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
